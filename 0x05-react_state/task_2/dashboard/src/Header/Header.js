3e3a6000c942ae391e9e2ebc26a7a24372da60da
Haythem <heythem.94@gmail.com>
2024-11-18 19:56:52 +0100

    fix

--- 0x05-react_state/task_2/dashboard/src/Header/Header.js
+++ 0x05-react_state/task_2/dashboard/src/Header/Header.js
@@ -1,49 +1,55 @@
-import React, { useContext } from "react";
-import logo from "../assets/holberton-logo.jpg";
-import { StyleSheet, css } from "aphrodite";
-import { AppContext } from "../App/AppContext";
+import React, { useContext } from 'react';
+import logo from '../assets/holberton_logo.jpg';
+import { StyleSheet, css } from 'aphrodite';
+import { AppContext } from '../App/AppContext';
 
 function Header() {
   const { user, logOut } = useContext(AppContext);
 
   return (
     <>
-      <div className={css(styles["App-header"])}>
-        <img src={logo} className={css(styles.img)} alt="logo" />
-        <h1>School dashboard</h1>
-      </div>
+      <header className={css(headerStyles.appHeader)}>
+        <img src={logo} alt="logo" className={css(headerStyles.appLogo)}/>
+        <h1 className={css(headerStyles.h1)}>School dashboard</h1>
+      </header>
 
-      {user.isLoggedIn && (
-        <section className={css(styles.greeting)} id="logoutSection">
-          Welcome<strong> {user.email} </strong>
-          <em>
-            <a href="#" onClick={logOut}>
-              (logout)
-            </a>
-          </em>
-        </section>
-      )}
+      {
+      user.isLoggedIn && <section id="logoutSection">
+        <h2>Welcome<strong> {user.email} </strong><em><a href="#" onClick={logOut}>(logout)</a></em>
+        </h2>
+      </section>
+      }
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  "App-header": {
-    fontSize: "1.4rem",
-    color: "#e0354b",
-    display: "flex",
-    alignItems: "center",
-    borderBottom: "3px solid #e0354b",
-  },
+const headerStyles = StyleSheet.create({
+	h1: {
+		marginLeft: '10rem',
+    float: 'right',
+    flex: 2,
+    '@media (max-width: 900px)': {
+      margin: 'auto'
+    }
+	},
 
-  img: {
-    width: "200px",
-    height: "200px",
-  },
+	appHeader: {
+		display: 'flex',
+		flexDirection: 'row',
+		alignItems: 'center',
+		color: '#E11D3F',
+		borderBottom: '1px solid #E11D3F',
+    width: '100%',
+    boxSizing: 'border-box'
+	},
 
-  greeting: {
-    marginTop: "1rem",
-  },
+	appLogo: {
+		maxHeight: '200px',
+		maxWidth: '200px',
+    height: 'auto',
+    width: 'auto',
+    flex: 1
+	}
 });
 
 export default Header;
